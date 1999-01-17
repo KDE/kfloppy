9c44ae9f0c22d1c622110939aec7953877a6f477
Hans Petter Bieker <bieker@kde.org>
1999-01-17 20:22:31 +0000

    i18n("OK")
    svn path=/trunk/kdeutils/kfloppy/; revision=15513

--- about.cpp
+++ about.cpp
@@ -66,7 +66,7 @@ MyAbout::MyAbout(QWidget *parent) : QDialog(parent, "About KFloppy Formatter", T
   f1->setGeometry(10,180,330,4);
 
 
-  QPushButton *b_ok = new QPushButton("OK", this);
+  QPushButton *b_ok = new QPushButton(i18n("OK"), this);
   b_ok->setGeometry(width()/2-40, height() - 48, 80, 32);
   b_ok->setDefault(TRUE);
   b_ok->setAutoDefault(TRUE);
