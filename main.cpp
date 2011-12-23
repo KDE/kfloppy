3bbfb98ba24945107eda2b3ab4b98bf41b3bafe3
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2011-12-22 23:47:29 -0200

    Bump version number now that 4.8 has been branched

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main( int argc, char *argv[] )
 {
   KAboutData aboutData("kfloppy", 0,
 	ki18n("KFloppy"),
-    "4.6.4", ki18n(description), KAboutData::License_GPL,
+    "4.8 rc2", ki18n(description), KAboutData::License_GPL,
     ki18n("(c) 1997, Bernd Johannes Wuebben\n"
     "(c) 2001, Chris Howells\n"
     "(c) 2002, Adriaan de Groot\n"
