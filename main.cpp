54d2cc5733146b3bdb03ebd64dacc5889c4c8d77
Wolfgang Bauer <wbauer@tmo.at>
2015-12-09 15:48:01 +0100

    Change version from "4.9 pre" to "4.9"
    It is no pre-release version any more, and will be the last KDE4 based
    release anyway... ;-)

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main( int argc, char *argv[] )
 {
   KAboutData aboutData("kfloppy", 0,
 	ki18n("KFloppy"),
-    "4.9 pre", ki18n(description), KAboutData::License_GPL,
+    "4.9", ki18n(description), KAboutData::License_GPL,
     ki18n("(c) 1997, Bernd Johannes Wuebben\n"
     "(c) 2001, Chris Howells\n"
     "(c) 2002, Adriaan de Groot\n"
