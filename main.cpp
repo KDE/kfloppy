3e5f08da5c7765fa3c19b6ae27e7708910d92308
Balló György <ballogyor@gmail.com>
2026-01-22 19:19:54 +0000

    Set window icon
    This is commonly used by desktop environments on Xorg.

--- main.cpp
+++ main.cpp
@@ -69,6 +69,8 @@ int main(int argc, char *argv[])
     aboutData.setTranslator(i18nc("NAME OF TRANSLATORS", "Your names"), i18nc("EMAIL OF TRANSLATORS", "Your emails"));
     KAboutData::setApplicationData(aboutData);
 
+    QApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("kfloppy")));
+
     KCrash::initialize();
 
     QCommandLineParser parser;
